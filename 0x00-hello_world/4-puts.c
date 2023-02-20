17fdc8b9bcb4f658c8fb771261e27c1b9283f318
jennybrian <omondibrian231@gmail.com>
2023-02-19 21:38:38 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints a line suing put function
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
