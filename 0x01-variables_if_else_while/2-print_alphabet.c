a16058ed4feb96da1617dd5e8949122cbb68b6f9
jennybrian <omondibrian231@gmail.com>
2023-02-20 01:24:34 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the Alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	char C;
+
+	for (C = 'a'; C <= 'z'; C++)
+	putchar(C);
+	putchar('\n');
+	return (0);
+}
