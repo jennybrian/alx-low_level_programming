7fa714d41080ffc750511aaa2b95fcaf59c5be39
jennybrian <omondibrian231@gmail.com>
2023-02-22 07:07:12 -0800

    list natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints sum of multiples of 3 or 5 upto 1024
+ * Return: Always
+ */
+
+int main(void)
+{
+	int 1, z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{ z += i;
+		}
+		i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
