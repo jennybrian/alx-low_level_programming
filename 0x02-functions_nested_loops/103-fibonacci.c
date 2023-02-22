a78667c44bf47edea6bb3ee6e37e8eb6ecf887bb
jennybrian <omondibrian231@gmail.com>
2023-02-22 07:39:57 -0800

    Even Liber Abbaci task

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of even Fibonacci number
+ * less than 4,000,000,
+ * Return: nothing
+ */
+
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2, sum = k;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+		if (k % 2 == 0)
+			sum += k;
+		j = k - j;
+		++1;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
